f345acdc1afb482702ab479e6006413e3739e2d8
Adrian Catalin <adrian.mereuta@gmail.com>
2024-07-29 20:53:17 +0300

    Am modificat variabila nume in name

--- src/obiecte_clase/Dog.java
+++ src/obiecte_clase/Dog.java
@@ -4,7 +4,7 @@ public class Dog {
     //proprietati:
     String rasa;
     int varsta;
-    public String nume;
+    public String name;
 
     //constructori
     public Dog(){
@@ -19,20 +19,20 @@ public class Dog {
 
         this.rasa = rasa;
         this.varsta = varsta;
-        this.nume = nume;
+        this.name = nume;
 
     }
     // metode
     public String toString(){
         // metoda toString () transforma obiectul nostru intr-un text
         // atunci cand dorim sa afisam un obiect folosind System.out.println metoda toString se apeleaza automat
-        return "Rasa: " + this.rasa + ", Varsta: " + this.varsta + ", Nume: " + this.nume;
+        return "Rasa: " + this.rasa + ", Varsta: " + this.varsta + ", Nume: " + this.name;
     }
 
     public boolean equals(Object o){
         Dog obj = (Dog) o; // am facut cast de la object la Dog
 
-        if (this.nume.equals(obj.nume) && this.rasa.equals(obj.rasa) && this.varsta == obj.varsta){
+        if (this.name.equals(obj.name) && this.rasa.equals(obj.rasa) && this.varsta == obj.varsta){
             return true;
         } else {return false;}
 
