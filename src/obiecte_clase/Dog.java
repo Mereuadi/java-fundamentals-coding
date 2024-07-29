19b6e27d89dd566a9ad926dbb9a9112fd9d1b8e2
Adrian Catalin <adrian.mereuta@gmail.com>
2024-07-29 20:45:22 +0300

    Am creat clasa Dog si clasa Main

---
+++ src/obiecte_clase/Dog.java
@@ -0,0 +1,41 @@
+package obiecte_clase;
+
+public class Dog {
+    //proprietati:
+    String rasa;
+    int varsta;
+    public String nume;
+
+    //constructori
+    public Dog(){
+        // constructor default
+        // acest constructor se genereaza automat si nu este vizibil in cod
+        // atunci cand generam un constructor, constructorul default DISPARE
+        // daca avem un constructor definit, putem defini in mod explicit constructorul default
+    }
+
+    public Dog(String rasa, int varsta, String nume){
+        // constructor cu parametri
+
+        this.rasa = rasa;
+        this.varsta = varsta;
+        this.nume = nume;
+
+    }
+    // metode
+    public String toString(){
+        // metoda toString () transforma obiectul nostru intr-un text
+        // atunci cand dorim sa afisam un obiect folosind System.out.println metoda toString se apeleaza automat
+        return "Rasa: " + this.rasa + ", Varsta: " + this.varsta + ", Nume: " + this.nume;
+    }
+
+    public boolean equals(Object o){
+        Dog obj = (Dog) o; // am facut cast de la object la Dog
+
+        if (this.nume.equals(obj.nume) && this.rasa.equals(obj.rasa) && this.varsta == obj.varsta){
+            return true;
+        } else {return false;}
+
+        }
+    }
+
